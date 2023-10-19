05ea66ff619b3d0f669dc7f6eff21b71eb192c11
raheemadamboev <vividricemaryandmax@gmail.com>
2023-10-19 10:38:30 +0500

    Implemented MainScreen;

---
+++ app/src/main/java/xyz/teamgravity/manual_dependency_injection/presentation/screen/MainScreen.kt
@@ -0,0 +1,11 @@
+package xyz.teamgravity.manual_dependency_injection.presentation.screen
+
+import androidx.compose.runtime.Composable
+import xyz.teamgravity.manual_dependency_injection.presentation.viewmodel.MainViewModel
+
+@Composable
+fun MainScreen(
+    viewmodel: MainViewModel
+) {
+
+}
